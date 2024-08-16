6be4b24c6373d51fab1ccd3c3ebe599e69f0341c
Lucas <lucathos012@hotmail.com>
2024-08-16 10:08:54 -0300

    V.1.2 criando home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 export default function Home(){
     return(
         <div>
             <h1>MEU SITE ESTA<strong>ONLINE</strong></h1>
+            <h2>Va para <Link to={'/login'}>login</Link></h2>
         </div>
     )
 }
