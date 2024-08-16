e9d0b268e92dc5cef2b24a080f569dd6d9f25599
Lucas <lucathos012@hotmail.com>
2024-08-16 15:49:26 -0300

    v.1.0 Beta

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,14 @@ import cors from 'cors'
 import auth from './middleware/auth.js'
 
 const app = express();
-app.use(express.json())
 app.use(cors({
-    origin: 'https://lucasnfagr.netlify.app', // URL do seu frontend hospedado no Netlify
+    origin: 'https://lucasnfagr.netlify.app/', // URL do seu frontend hospedado no Netlify
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adicione os métodos permitidos
     credentials: true // Se estiver usando cookies ou autenticação via token
 }));
 
 
+app.use(express.json())
 app.use('/', olaRoute)
 app.use('/', auth, privateRoute)
 
