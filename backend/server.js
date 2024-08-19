b947069752d376de685a92ca9ac75470a16dd1f6
Lucas <lucathos012@hotmail.com>
2024-08-19 10:04:48 -0300

    V.0.2

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,18 @@ import auth from './middleware/auth.js';
 const app = express();
 
 // Middleware de CORS
-app.use(cors({
-    origin: 'https://lucasnfagr.netlify.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://lucasnfagr.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    // Para lidar com requisições preflight
+    if (req.method === 'OPTIONS') {
+        return res.status(200).json({});
+    }
+
+    next();
+});
 
 // Middleware para JSON
 app.use(express.json());
