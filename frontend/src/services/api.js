4f3ed416672cc48102e2db571817ae6c1781a871
Lucas <lucathos012@hotmail.com>
2024-08-19 10:10:49 -0300

    V.0.2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://nf-umber.vercel.app/'
+    baseURL: 'https://nf-rlacu8buq-lucas-projects-1129442c.vercel.app/'
 })
 
 export default api
