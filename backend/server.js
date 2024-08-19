967d168b1ad508bbab609b9ecd022ec10cce7247
Lucas <lucathos012@hotmail.com>
2024-08-19 13:39:03 -0300

    new v1

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,9 @@ import auth from './middleware/auth.js';
 
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:5173',  // Domínio de desenvolvimento
-    'https://lucasnfagr.netlify.app' // Domínio de produção
-];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'https://lucasnfagr.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
