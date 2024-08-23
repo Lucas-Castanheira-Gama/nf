3f07f77cb1623322253b9b7785fbb77708050966
Lucas_Gama <Lucathos012@hotmail.com>
2024-08-22 22:55:10 -0300

    v01

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Permite conexões de todas as interfaces
+    port: 5173,        // Porta em que o Vite deve escutar
+    strictPort: true,  // Garante que a porta especificada seja usada
     watch: {
       usePolling: true,
     },
-    host:true,
-    strictPort:true,
-
   },
 })
